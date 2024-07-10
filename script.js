292200b5af373a83b117cb086edea6eebff4b292
Luiz Leal <143563882+cainhurst87@users.noreply.github.com>
2024-07-10 10:29:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+// pre-generate the password on site startup
+// print it on textarea
+
+
+//warn if no parameters are selected  
+
+// update char count when lenght is changed 
+
+
+// start password generation on "regenerate" input
+// print it on textarea
+
+// fetch password parameters     
+
+
+// actually generating the password
+
+// copy password text on "copy" input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
